11e4503e8ef0f3976f7136ddd92f8dd20a236415
ritusoni2298 <ritusoni2298@gmail.com>
2019-09-05 14:30:30 -0400

    one more commit

--- 123/main.js
+++ 123/main.js
@@ -3,6 +3,8 @@ var x=0
     for (var i = 0; i < 5; i++) {
         console.log('inside for' + i);
     }
-    console.log('finaly' + i+'x'+x);
+    
+console.log('finaly' + i+'x'+x);
+//https://github.com/ritusoni2298/structuredFile.git
 }
 dosomething();
